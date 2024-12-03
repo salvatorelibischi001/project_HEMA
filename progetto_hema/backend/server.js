3980a9215e970bc2666a6be0c66f9ac2c4d5f77f
salvatorelibischi001 <salvatore.libischi001@studenti.uniparthenope.it>
2024-12-03 12:10:38 +0100

    modificato

--- progetto_hema/backend/server.js
+++ progetto_hema/backend/server.js
@@ -1,3 +1,4 @@
+//Il server è il cuore del backend, connette MongoDB usando mongoose. Inoltre, gestisce le varie rotte configura Express e Node.JS;
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,7 +8,7 @@ const rotteautenticazione = require('./rotte/rotteautenticazione');
 
 const app = express();
 
-// Middleware
+// Middleware: serve a tradurre i dati JSON leggibili per JavaScript.
 app.use(express.json());
 app.use(cors());
 
@@ -15,12 +16,12 @@ app.use(cors());
 mongoose
     .connect('mongodb://localhost:27017/hema', { useNewUrlParser: true, useUnifiedTopology: true })
 
-// Rotte
+// Middleware per le rotte.
 app.use('/api/auth', authRoutes);
 app.use('/api/prenotazione', prenotazioneRoutes);//Si occupa della comunicazione di prenotazioni.
 app.use('/api/auth', rotteautenticazione);//Si occupa del login e registrazione utente.
 
-// Avvio del server, uso la porta 4000
+// Avvio del server, uso la porta 4000.
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server in ascolto su http://localhost:${PORT}`);
