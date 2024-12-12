a87c77d5c2c1a8bd2837d3a6df125129a9b0f14f
salvatorelibischi001 <salvatore.libischi001@studenti.uniparthenope.it>
2024-12-12 19:27:05 +0100

    Modificato sal

--- progetto_hema/backend/server.js
+++ progetto_hema/backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./rotte/rotteautenticazione');
 const prenotazioneRoutes = require('./rotte/rotteprenotazione');
-const rotteautenticazione = require('./rotte/rotteautenticazione');
+const Utente = require('./modelli/utente');
 
 const app = express();
 
@@ -12,14 +12,52 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connessione a MongoDB: stabilisco la connesione mediante mongooose.
+// Connessione a MongoDB: stabilisco la connessione mediante mongoose.
 mongoose
-    .connect('mongodb://localhost:27017/hema', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect('mongodb://localhost:27017/hema')
+    console.log('Connesso a MongoDB')
 
 // Middleware per le rotte.
 app.use('/api/auth', authRoutes);
-app.use('/api/prenotazione', prenotazioneRoutes);//Si occupa della comunicazione di prenotazioni.
-app.use('/api/auth', rotteautenticazione);//Si occupa del login e registrazione utente.
+app.use('/api/prenotazione', prenotazioneRoutes);
+
+// Rotta per ottenere informazioni utente
+app.get('/api/user/info', async (req, res) => {
+    try {
+        const email = req.query.email; // Recupera l'email dalla query 
+
+        const utente = await Utente.findOne({ email });
+        if (!utente) {
+            return res.status(404).json({ message: 'Utente non trovato' });
+        }
+
+        res.status(200).json({
+            nome: utente.nome,
+            email: utente.email,
+            gruppoSanguigno: utente.gruppoSanguigno,
+        });
+    } catch (err) {
+        console.error('Errore durante il recupero delle informazioni utente:', err);
+        res.status(500).json({ message: 'Errore del server' });
+    }
+});
+
+// Rotta per ottenere lo storico delle prenotazioni. Da modificare per dare dinamicità.
+app.get('/api/user/prenotazioni', async (req, res) => {
+    try {
+        const emailUtente = 'salvatore.libi@gmail.com'; // Temporaneamente impostato statico
+        const prenotazioni = await Prenotazione.find({ emailUtente });
+
+        if (!prenotazioni || prenotazioni.length === 0) {
+            return res.status(200).json([]); // Nessuna prenotazione trovata
+        }
+
+        res.status(200).json(prenotazioni);
+    } catch (err) {
+        console.error('Errore durante il recupero delle prenotazioni:', err);
+        res.status(500).json({ message: 'Errore del server durante il recupero delle prenotazioni.' });
+    }
+});
 
 // Avvio del server, uso la porta 4000.
 const PORT = 4000;
